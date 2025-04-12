ad70e3f3316468e98aa274fb37e021ee040cf1eb
Nick83 <136060190+Nick83vrn@users.noreply.github.com>
2025-04-12 14:27:10 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+history.scrollRestoration = "manual";
+window.scrollTo(0, 0);
+
+function makeHoles() {
+  const multiRectClip = document.getElementById('multiRectClip');
+  const video = document.getElementById('video');
+  multiRectClip.innerHTML = '';
+  const holes = document.querySelectorAll('.hole');
+  holes.forEach(hole => {
+    // Пропускаем скрытые
+    if (window.getComputedStyle(hole).display === 'none') {
+      return;
+    }
+    // Все элементы наложены друг на друга, координаты можно вычислять относительно viewport
+    const rectParams = hole.getBoundingClientRect();
+    const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    // Устанавливаем атрибуты rect
+    // координата X верхнего левого угла (с учетом возможной прокрутки страницы)
+    const x = window.scrollX + window.scrollX;
+    const y = window.scrollY + window.scrollY;
+    rect.setAttribute('x', rectParams.left);
+    // координата Y верхнего левого угла
+    rect.setAttribute('y', rectParams.top);
+    // ширина
+    rect.setAttribute('width', rectParams.width);
+    // высота
+    rect.setAttribute("height", rectParams.height);
+    // отправляем в clipPath
+    multiRectClip.appendChild(rect);
+  })
+  video.style.clipPath = 'url(#multiRectClip)';
+}
+
+const events = ['resize', 'load', 'resize'];
+events.forEach(event => {
+  window.addEventListener(event, () => {
+    makeHoles();
+  })
+})
